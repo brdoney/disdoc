14ee1720b36f2aa0be8cb362082d54fc4023e2de
Brendan Doney <bre.doney@gmail.com>
2024-04-16 14:38:22 -0400

    Default to using mock LLM

--- llm.py
+++ llm.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import re
 from collections.abc import AsyncIterable
@@ -140,14 +141,29 @@ async def openai(
     print(f"Generation: {end - start}s")
 
 
+async def mock(*_) -> AsyncIterable[str]:
+    """Mock LLM that yields words from a predefined message. Accepts arguments to be
+    interchangeable with other LLMs, but doesn't use them."""
+
+    mock_message = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eu tellus lorem. Nulla ornare felis eleifend consequat tempus. Donec quam libero, dapibus ac dolor ac, bibendum aliquam neque. Aliquam erat volutpat. Quisque erat leo, dictum imperdiet neque non, vulputate lacinia diam. Fusce tincidunt urna et nibh placerat varius. Donec semper arcu et finibus ornare."""
+    content = ""
+    for word in mock_message.split():
+        await asyncio.sleep(0.02)
+        content += f"{word} "
+        yield content
+
+
 class LLMType(Enum):
     LLAMA = auto()
     OPENAI = auto()
+    MOCK = auto()
 
     def __call__(self, question: str, context_docs: list[tuple[Document, float]]):
-        if self == LLMType.LLAMA:
+        if self is LLMType.LLAMA:
             return llama(question, context_docs)
-        elif self == LLMType.OPENAI:
+        elif self is LLMType.OPENAI:
             return openai(question, context_docs)
+        elif self is LLMType.MOCK:
+            return mock(question, context_docs)
         else:
             raise ValueError(f"Unsupported LLMType: {self.name}")
