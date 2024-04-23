051dc493e949ebde2855a44d3114589b2604fc6d
Brendan Doney <bre.doney@gmail.com>
2024-04-23 01:19:58 -0400

    Finish scheduler types

--- main.py
+++ main.py
@@ -246,7 +246,18 @@ def ask(question: str, category: DocGroup):
     return record.to_json(), 200  # type: ignore[reportUnknownMemberType]
 
 
-# _ = schedman.CFS(["scx_rustland", "scx_rusty"], 0.75)
+# Frontend and backend, respectively
+best_scheds = ["scx_rustland", "scx_rusty"]
+frontend_s = 16.68
+backend_s = 3.14
+frontend_benefit = 12.29
+backend_benefit = 2.92
+# _ = schedman.CFS(best_scheds, 0.75)
+_ = schedman.DeltaWeightedCFS(best_scheds, frontend_s, backend_s, 1.8)  # BEST RESULTS
+# _ = schedman.BenefitWeightedCFS(best_scheds, frontend_benefit, backend_benefit, 1)
+# _ = schedman.DeltaBenefitWeightedCFS(
+#     best_scheds, frontend_s, frontend_benefit, backend_s, backend_benefit, 2.1
+# )
 
 if __name__ == "__main__":
     app.run()
--- schedman.py
+++ schedman.py
@@ -48,9 +48,12 @@ def switch_scheduler(
     return start_scheduler(new_sched)
 
 
-class CFS:
+class WeightedCFS:
+    """General purpose class for weighted CFS"""
+
     timeslice_s: float
     scheds: list[str]
+    weights: list[float]
 
     timer: threading.Timer
 
@@ -58,13 +61,17 @@ class CFS:
     curr_sched: str
     curr_p_sched: subprocess.Popen[bytes]
 
-    def __init__(self, scheds: list[str], timeslice_s: float = 0.5) -> None:
+    def __init__(
+        self, scheds: list[str], weights: list[float], timeslice_s: float
+    ) -> None:
         super().__init__()
         # Starting info
         self.timeslice_s = timeslice_s
         self.scheds = scheds
         self.curr_i = 0
         self.curr_sched = scheds[self.curr_i]
+        self.weights = weights
+        print("weights", [timeslice_s * w for w in weights])
         # Start desired sched
         self.curr_p_sched = start_scheduler(self.curr_sched)
 
@@ -73,7 +80,9 @@ class CFS:
         self.timer.start()
 
     def _create_timer(self) -> threading.Timer:
-        return threading.Timer(self.timeslice_s, self._update)
+        return threading.Timer(
+            self.timeslice_s * self.weights[self.curr_i], self._update
+        )
 
     def _update(self) -> None:
         self.curr_i = (self.curr_i + 1) % len(self.scheds)
@@ -82,3 +91,87 @@ class CFS:
         self.curr_p_sched = switch_scheduler(self.curr_sched, self.curr_p_sched)
         self.timer = self._create_timer()
         self.timer.start()
+
+
+class CFS(WeightedCFS):
+    def __init__(self, scheds: list[str], timeslice_s: float = 0.75) -> None:
+        super().__init__(scheds, [1.0] * len(scheds), timeslice_s)
+
+
+class DeltaWeightedCFS(WeightedCFS):
+    """Prioritises the system with the lowest throughput"""
+
+    def __init__(
+        self,
+        scheds: list[str],
+        frontend_s: float,
+        backend_s: float,
+        timeslice_s: float = 2,
+    ) -> None:
+        denom = frontend_s + backend_s
+        # First we find how much each system contributes to overall throughput,
+        # then invert so most time-constrained gets highest share
+        weights = [1 - frontend_s / denom, 1 - backend_s / denom]
+        print(weights)
+        # These are shares of the comined timeslice (e.g. 75% of the combined timeslices)
+        combined_timeslice = 2 * timeslice_s
+        super().__init__(scheds, weights, combined_timeslice)
+
+
+class BenefitWeightedCFS(WeightedCFS):
+    """Prioritises the system with the highest benefit"""
+
+    def __init__(
+        self,
+        scheds: list[str],
+        frontend_benefit: float,
+        backend_benefit: float,
+        timeslice_s: float = 0.75,
+    ) -> None:
+        denom = frontend_benefit + backend_benefit
+        weights = [frontend_benefit / denom, backend_benefit / denom]
+        print(weights)
+        # These are shares of the combined timeslice
+        combined_timeslice = 2 * timeslice_s
+        super().__init__(scheds, weights, combined_timeslice)
+
+
+class DeltaBenefitWeightedCFS(WeightedCFS):
+    """Prioritises the system with the highest benefit and lowest throughput"""
+
+    def __init__(
+        self,
+        scheds: list[str],
+        frontend_s: float,
+        frontend_benefit: float,
+        backend_s: float,
+        backend_benefit: float,
+        timeslice_s: float = 0.75,
+    ) -> None:
+        # See DeltaWeighted for explanation
+        delta_denom = frontend_s + backend_s
+        delta_weights = [1 - frontend_s / delta_denom, 1 - backend_s / delta_denom]
+
+        # See BenefitWeighted for explanation
+        benefit_denom = frontend_benefit + backend_benefit
+        benefit_weights = [
+            frontend_benefit / benefit_denom,
+            backend_benefit / benefit_denom,
+        ]
+
+        weights = [
+            delta_weights[0] * benefit_weights[0],
+            delta_weights[1] * benefit_weights[1],
+        ]
+
+        # Standardize it (e.g. 0.6*0.2 + 0.4*0.8 != 1.0 anymore)
+        total = sum(weights)
+        weights = [
+            weights[0] / total,
+            weights[1] / total,
+        ]
+        print(weights)
+
+        # These are shares of the combined timeslice
+        combined_timeslice = 2 * timeslice_s
+        super().__init__(scheds, weights, combined_timeslice)
